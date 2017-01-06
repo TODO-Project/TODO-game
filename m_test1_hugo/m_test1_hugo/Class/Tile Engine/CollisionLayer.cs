efacbe3a7271679f492515459ac5e9f4c22d641e
FennecStudent <louis.boulanger@etu.univ-smb.fr>
2017-01-06 08:54:01 +0100

    Mise à jour du fonctionnement de la collision en passant d'un enum à des boolens

--- m_test1_hugo/m_test1_hugo/Class/Tile Engine/CollisionLayer.cs
+++ m_test1_hugo/m_test1_hugo/Class/Tile Engine/CollisionLayer.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace m_test1_hugo.Class.Tile_Engine
+namespace m_test1_hugo.Class
 {
-    // Création du type de collision : passable ou impassable
-    public enum CollisionType { Passable, Unpassable };
-
+    
     class CollisionLayer
     {
         #region Fields
 
-        CollisionType[,] map;
+        bool[,] map;
 
         #endregion
 
@@ -25,13 +23,13 @@ namespace m_test1_hugo.Class.Tile_Engine
 
         public CollisionLayer(int height, int width)
         {
-            map = new CollisionType[height, width];
+            map = new bool[height, width];
 
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    SetTile(x, y, CollisionType.Passable);
+                    SetTile(x, y, true);
                 }
             }
         }
@@ -45,8 +43,8 @@ namespace m_test1_hugo.Class.Tile_Engine
         /// </summary>
         /// <param name="x">Coordonnée x de la tile</param>
         /// <param name="y">Coordonnée y de la tile</param>
-        /// <returns>Une valeur CollisionType (passable ou unpassable)</returns>
-        public CollisionType GetTile(int x, int y)
+        /// <returns>Une valeur CollisionType (true or false)</returns>
+        public bool GetTile(int x, int y)
         {
             return map[y, x];
         }
@@ -56,8 +54,8 @@ namespace m_test1_hugo.Class.Tile_Engine
         /// </summary>
         /// <param name="x">Coordonnée x de la tile</param>
         /// <param name="y">Coordonnée y de la tile</param>
-        /// <param name="value">Valeur de la collision (passable ou unpassable)</param>
-        public void SetTile(int x, int y, CollisionType value)
+        /// <param name="value">Valeur de la collision (true ou false)</param>
+        public void SetTile(int x, int y, bool value)
         {
             map[y, x] = value;
         }
