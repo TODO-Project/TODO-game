9c154fb922275f140a1976213030f65301353e20
FennecStudent <louis.boulanger@etu.univ-smb.fr>
2017-01-06 09:37:55 +0100

    Ajout de l'interface Collision pour gérer la collision entre le joueur et les éléments du jeu

---
+++ m_test1_hugo/m_test1_hugo/Class/Main/interfaces/Collision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m_test1_hugo.Class.Main.interfaces
+{
+    interface Collision
+    {
+        bool TileCollision(Sprite objet1);
+
+        bool SpriteCollision(Sprite objet1, Sprite objet2); // objet 2 devra etre un rectangle pour utiliser Bounds.Intersects
+    }
+}
